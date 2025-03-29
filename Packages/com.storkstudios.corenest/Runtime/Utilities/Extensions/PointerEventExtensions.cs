d5bef35d7f8d8d65871e2ca9d0fdf45578a31d30
goblon17 <81117560+goblon17@users.noreply.github.com>
2025-03-29 16:49:01 +0100

    Create pointer event extensions

---
+++ Packages/com.storkstudios.corenest/Runtime/Utilities/Extensions/PointerEventExtensions.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class PointerEventExtensions
+{
+    private static List<RaycastResult> raycastResults = new List<RaycastResult>();
+
+    public static GameObject GetNextRaycastTarget(this PointerEventData eventData, GameObject current)
+    {
+        raycastResults.Clear();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+        int currentIndex = raycastResults.FindIndex(e => e.gameObject == current);
+        int nextObject = currentIndex + 1;
+        if (nextObject >= raycastResults.Count)
+        {
+            return null;
+        }
+        return raycastResults[nextObject].gameObject;
+    }
+}
