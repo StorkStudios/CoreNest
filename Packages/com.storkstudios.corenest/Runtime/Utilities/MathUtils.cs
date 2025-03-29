951fca49053cdff810423277d2ec5e839669a559
goblon17 <81117560+goblon17@users.noreply.github.com>
2025-03-29 16:47:51 +0100

    Add modulo funciton

--- Packages/com.storkstudios.corenest/Runtime/Utilities/MathUtils.cs
+++ Packages/com.storkstudios.corenest/Runtime/Utilities/MathUtils.cs
@@ -10,4 +10,9 @@ public static class MathUtils
         Vector3 AV = value - a;
         return Vector3.Dot(AV, AB) / Vector3.Dot(AB, AB);
     }
+
+    public static int Mod(int v, int d)
+    {
+        return ((v % d) + d) % d;
+    }
 }
