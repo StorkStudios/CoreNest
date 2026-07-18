7defd493613fcd9279ef301808d98fcacbad10d1
unknown <andrzej.gauza@outlook.com>
2026-07-18 11:25:11 +0200

    Add normalization and derivative

--- Runtime/Extensions/AnimationCurveExtensions.cs
+++ Runtime/Extensions/AnimationCurveExtensions.cs
@@ -5,6 +5,13 @@ namespace StorkStudios.CoreNest
 {
     public static class AnimationCurveExtensions
     {
+        [System.Flags]
+        public enum NormalizationAxis
+        {
+            X = 1,
+            Y = 2
+        }
+
         public static float EvaluateUnclamped(this AnimationCurve curve, float time)
         {
             Keyframe first = curve.keys.First();
@@ -16,9 +23,9 @@ namespace StorkStudios.CoreNest
             return time < first.time ? first.value : last.value;
         }
 
-        public static AnimationCurve GetNormalizedAnimationCurve(this AnimationCurve curve)
+        public static AnimationCurve GetNormalizedAnimationCurve(this AnimationCurve curve, NormalizationAxis axes = NormalizationAxis.X | NormalizationAxis.Y)
         {
-            AnimationCurve result = new AnimationCurve();
+            AnimationCurve result = new();
 
             if (curve == null || curve.length < 1)
             {
@@ -34,32 +41,66 @@ namespace StorkStudios.CoreNest
 
             foreach (Keyframe key in curve.keys[1..])
             {
-                if (key.time < minKeyTime)
+                if (axes.HasFlag(NormalizationAxis.X))
                 {
-                    minKeyTime = key.time;
+                    if (key.time < minKeyTime)
+                    {
+                        minKeyTime = key.time;
+                    }
+                    if (key.time > maxKeyTime)
+                    {
+                        maxKeyTime = key.time;
+                    }
                 }
-                if (key.time > maxKeyTime)
+                if (axes.HasFlag(NormalizationAxis.Y))
                 {
-                    maxKeyTime = key.time;
-                }
-                if (key.value > maxKeyValue)
-                {
-                    maxKeyValue = key.value;
-                }
-                if (key.value < minKeyValue)
-                {
-                    minKeyValue = key.value;
+                    if (key.value > maxKeyValue)
+                    {
+                        maxKeyValue = key.value;
+                    }
+                    if (key.value < minKeyValue)
+                    {
+                        minKeyValue = key.value;
+                    }
                 }
             }
 
             foreach (Keyframe key in curve.keys)
             {
                 Keyframe normalizedKey = key;
-                normalizedKey.time = Mathf.InverseLerp(minKeyTime, maxKeyTime, key.time);
-                normalizedKey.value = Mathf.InverseLerp(minKeyValue, maxKeyValue, key.value);
+                if (axes.HasFlag(NormalizationAxis.X))
+                {
+                    normalizedKey.time = Mathf.InverseLerp(minKeyTime, maxKeyTime, key.time);
+                }
+                if (axes.HasFlag(NormalizationAxis.Y))
+                {
+                    normalizedKey.value = Mathf.InverseLerp(minKeyValue, maxKeyValue, key.value);
+                }
                 result.AddKey(normalizedKey);
             }
             return result;
+        }
+
+        public static float GetDerivativeAt(this AnimationCurve curve, float time, float eps = 0.001f)
+        {
+            if (curve == null || curve.length < 1)
+            {
+                return 0f;
+            }
+
+            float start = curve.keys[0].time;
+            float end = curve.keys[curve.length - 1].time;
+
+            float x1 = Mathf.Max(start, time - eps);
+            float x2 = Mathf.Min(end, time + eps);
+
+            if (Mathf.Approximately(x1, x2))
+            {
+                return 0f;
+            }
+
+            return (curve.Evaluate(x2) - curve.Evaluate(x1)) / (x2 - x1);
+
         }
     }
 }
