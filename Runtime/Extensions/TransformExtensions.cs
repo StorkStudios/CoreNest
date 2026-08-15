c8cba01c3a5fe239b215a4dcd701ff62dfe036fa
Andrzej Gauza <31095662+Pojemnik@users.noreply.github.com>
2026-08-15 16:00:18 +0200

    Add transform extensions (#53)

---
+++ Runtime/Extensions/TransformExtensions.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace StorkStudios.CoreNest
+{
+    public static class TransformExtensions
+    {
+        /// <summary>
+        /// Interpolates transform's position, localScale and rotation using Unity's buildin Lerp methods.
+        /// <see cref="Vector3.Lerp"/>
+        /// <see cref="Quaternion.Lerp"/>
+        /// </summary>
+        /// <seealso cref="LerpUnclamped"/>
+        public static void Lerp(this Transform transform, Transform a, Transform b, float t)
+        {
+            transform.position = Vector3.Lerp(a.position, b.position, t);
+            transform.localScale = Vector3.Lerp(a.localScale, b.localScale, t);
+            transform.rotation = Quaternion.Lerp(a.rotation, b.rotation, t);
+        }
+
+        /// <summary>
+        /// Interpolates transform's position, localScale and rotation using Unity's buildin LerpUnclamped methods, allowing extrapolation beyond the end points.
+        /// <see cref="Vector3.LerpUnclamped"/>
+        /// <see cref="Quaternion.LerpUnclamped"/>
+        /// </summary>
+        /// <seealso cref="LerpUnclamped"/>
+        public static void LerpUnclamped(this Transform transform, Transform a, Transform b, float t)
+        {
+            transform.position = Vector3.LerpUnclamped(a.position, b.position, t);
+            transform.localScale = Vector3.LerpUnclamped(a.localScale, b.localScale, t);
+            transform.rotation = Quaternion.LerpUnclamped(a.rotation, b.rotation, t);
+        }
+    }
+}
